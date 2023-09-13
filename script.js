06b829c113d9fe2709f7e7c37f6a5ce51c843499
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-09-13 15:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	
+  let arr = [];
+	while(num!==0 || num!==1){
+		var biDi = num%2
+		arr.push(biDi);
+		num=Math.trunc(num/2);
+	}
+	let newArr = arr.join("");
+	let ans = Number(newArr);
+	return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
